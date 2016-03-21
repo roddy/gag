71c5f72e1847245ab6539f36b4f8911654021a28
Insights Service EC2 User <ec2@InsightsService>
2016-03-21 07:50:43 +0000

    Instrumented by Insights Service for uploading the artifacts

--- df-build.sh
+++ df-build.sh
@@ -10,4 +10,4 @@ zip libs.zip $(git ls-files -o | grep -e target/dependency/.*jar) || { echo "Err
 zip build.zip $(git ls-files -o | grep -e target/classes/.*class) || { echo "Error: Zipping binaries failed"; exit 1; }
 zip testbuild.zip $(git ls-files -o | grep -e target/test-classes/.*class) || { echo "Error: Zipping test binaries failed"; exit 1; }
 echo "Zipping artifacts DONE!!!"
-# Modified by Insights Service at 2016-03-20 14:21:21.879135
+# Modified by Insights Service at 2016-03-21 07:50:43.323496
